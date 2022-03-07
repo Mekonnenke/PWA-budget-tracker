22b7a8345b33b9ac5c8f25bce4ed504d06a589a4
Mona Lisa <mekotigi@gmail.com>
2022-03-07 00:50:56 -0500

    creat idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,8 +1,8 @@
-// create variable to hold db connection
+// db connection variable created here
 let db;
 
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
-const request = indexedDB.open('budget_tracker', 1);
+const request = indexedDB.open('PWA-budget_tracker', 1);
 
 // this event will emit if the database version changes
 request.onupgradeneeded = function(event) {
@@ -43,53 +43,3 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record);
 }
-
-function uploadTransaction() {
-
-    // open a transaction on your db
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-    // access your object store
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-
-    // get all records from store and set to a variable
-    const getAll = budgetObjectStore.getAll();
-
-    getAll.onsuccess = function() {
-
-        // if there was data in indexedDb's store send it to the api server
-        if (getAll.result.length > 0) {
-            fetch('/api/transaction', {
-                method: 'POST',
-                body: JSON.stringify(getAll.result),
-                headers: {
-                    Accept: 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then(response => response.json())
-                .then(serverResponse => {
-                    if (serverResponse.message) {
-                        throw new Error(serverResponse);
-                    }
-
-                    // open one more transaction
-                    const transaction = db.transaction(['new_transaction'], 'readwrite');
-
-                    // access the new_transaction object store
-                    const budgetObjectStore = transaction.objectStore('new_transaction');
-
-                    // clear all items in your store
-                    budgetObjectStore.clear();
-
-                    alert('All saved transactions has been submitted!');
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
-    }
-}
-
-// listen for app coming back online
-window.addEventListener('online', uploadTransaction);
